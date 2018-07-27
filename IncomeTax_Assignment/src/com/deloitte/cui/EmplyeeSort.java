e56f648f08bc43911e87042c99a125df59e883e4
sadhanak27 <sadhanak27@gmail.com>
2018-07-27 18:38:36 +0530

    First Commit

---
+++ IncomeTax_Assignment/src/com/deloitte/cui/EmplyeeSort.java
@@ -0,0 +1,19 @@
+package com.deloitte.cui;
+
+import java.util.Comparator;
+
+public class EmplyeeSort implements Comparator<EmplyeeVo> {
+
+	public EmplyeeSort() {
+
+	}
+
+	@Override
+	public int compare(EmplyeeVo e1, EmplyeeVo e2) {
+
+		return (int) (e1.getIncometa() - e2.getIncometa());
+	}
+
+	
+}
+
